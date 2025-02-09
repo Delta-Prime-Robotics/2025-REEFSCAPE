17cef84c82c9858656989d4564d7f2541d1b99b9
Liam Wilmot <114833225+itsameLuigi64@users.noreply.github.com>
2025-02-09 14:45:17 -0500

    Enhance VisionSubsystem with target pose retrieval and detection methods
    most importent part of this is the getUnreadResults is now a static variable. It says to only grab the unread results once per 20ms loop

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,15 +6,19 @@ package frc.robot.subsystems;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -25,6 +29,7 @@ public class VisionSubsystem extends DriveSubsystem {
   private PhotonCamera m_camera;
   private PhotonPoseEstimator m_photonEstimator;
   private Matrix<N3, N1> curStdDevs;
+  private static List<PhotonPipelineResult> currentPipelineResults;
   
     /** Creates a new VisionSubsystem. */
     public VisionSubsystem() {
@@ -50,7 +55,7 @@ public class VisionSubsystem extends DriveSubsystem {
    */
   public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
       Optional<EstimatedRobotPose> visionEst = Optional.empty();
-      for (var change : m_camera.getAllUnreadResults()) {
+      for (var change : getAllUnreadResultsStatic()) {
           visionEst = m_photonEstimator.update(change);
           updateEstimationStdDevs(visionEst, change.getTargets());
       }
@@ -64,46 +69,45 @@ public class VisionSubsystem extends DriveSubsystem {
    * @param estimatedPose The estimated pose to guess standard deviations for.
    * @param targets All targets in this camera frame
    */
-  private void updateEstimationStdDevs(
-          Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
-      if (estimatedPose.isEmpty()) {
-          // No pose input. Default to single-tag std devs
-          curStdDevs = kSingleTagStdDevs;
+  private void updateEstimationStdDevs(Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
+    if (estimatedPose.isEmpty()) {
+        // No pose input. Default to single-tag std devs
+        curStdDevs = kSingleTagStdDevs;
 
-      } else {
-          // Pose present. Start running Heuristic
-          var estStdDevs = kSingleTagStdDevs;
-          int numTags = 0;
-          double avgDist = 0;
+    } else {
+      // Pose present. Start running Heuristic
+      var estStdDevs = kSingleTagStdDevs;
+      int numTags = 0;
+      double avgDist = 0;
 
-          // Precalculation - see how many tags we found, and calculate an average-distance metric
-          for (var tgt : targets) {
-              var tagPose = m_photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
-              if (tagPose.isEmpty()) continue;
-              numTags++;
-              avgDist +=
-                      tagPose
-                              .get()
-                              .toPose2d()
-                              .getTranslation()
-                              .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
-          }
-
-          if (numTags == 0) {
-              // No tags visible. Default to single-tag std devs
-              curStdDevs = kSingleTagStdDevs;
-          } else {
-              // One or more tags visible, run the full heuristic.
-              avgDist /= numTags;
-              // Decrease std devs if multiple targets are visible
-              if (numTags > 1) estStdDevs = kMultiTagStdDevs;
-              // Increase std devs based on (average) distance
-              if (numTags == 1 && avgDist > 4)
-                  estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
-              else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
-              curStdDevs = estStdDevs;
-          }
+      // Precalculation - see how many tags we found, and calculate an average-distance metric
+      for (var tgt : targets) {
+          var tagPose = m_photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+          if (tagPose.isEmpty()) continue;
+          numTags++;
+          avgDist +=
+                  tagPose
+                          .get()
+                          .toPose2d()
+                          .getTranslation()
+                          .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
       }
+
+      if (numTags == 0) {
+          // No tags visible. Default to single-tag std devs
+          curStdDevs = kSingleTagStdDevs;
+      } else {
+          // One or more tags visible, run the full heuristic.
+          avgDist /= numTags;
+          // Decrease std devs if multiple targets are visible
+          if (numTags > 1) estStdDevs = kMultiTagStdDevs;
+          // Increase std devs based on (average) distance
+          if (numTags == 1 && avgDist > 4)
+              estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+          else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+          curStdDevs = estStdDevs;
+      }
+    }
   }
 
   /**
@@ -116,10 +120,80 @@ public class VisionSubsystem extends DriveSubsystem {
       return curStdDevs;
   }
 
+  /**
+   * Retrieves the pose of the first target that matches any of the specified target tags.
+   *
+   * @param targetTags A list of integer IDs representing the target tags to search for.
+   * @param allPipelineResults A list of PhotonPipelineResult objects containing the results from the vision pipeline.
+   * @return An Optional containing the Pose2d of the first matching target if found, otherwise an empty Optional.
+   */
+  public Optional<Pose2d> getTargetPose(List<Integer> targetTags, List<PhotonPipelineResult> allPipelineResults) {
+    List<PhotonPipelineResult> results = allPipelineResults;
+    for (PhotonPipelineResult result : results) {
+        if (!result.hasTargets()) continue;
+
+        return result.getTargets().stream()
+            .filter(target -> targetTags.contains(target.getFiducialId()))
+            .map(target -> new Pose2d(target.getBestCameraToTarget().getX(),
+                                      target.getBestCameraToTarget().getY(), 
+                                      new Rotation2d(target.getYaw())))
+            .findFirst();
+    }
+
+    return Optional.empty();
+  }
+
+  /**
+   * Checks if any of the detected tags are present in the target list.
+   *
+   * @param targetTags A list of target tag IDs to search for.
+   * @param allPipelineResults A list of PhotonPipelineResult objects containing the results from the vision pipeline.
+   * @return true if any of the detected tags are in the target list, false otherwise.
+   */
+  public boolean isTargetPresent(List<Integer> targetTags, List<PhotonPipelineResult> allPipelineResults) {
+    Optional<List<PhotonTrackedTarget>> targets = this.getTargets(allPipelineResults);
+    if(!targets.isEmpty());
+      List<PhotonTrackedTarget> result = targets.get();
+      // Get the list of detected tag IDs
+      List<Integer> detectedTagIds = 
+      result.stream()
+            .mapToInt(PhotonTrackedTarget::getFiducialId)
+            .boxed()
+            .collect(Collectors.toList());
+
+      // Check if any of the detected tags are in our target list
+      for (Integer tagId : detectedTagIds) {
+          if (targetTags.contains(tagId)) {
+              return true; // Found a matching tag
+          }
+    }
+    return false;
+}
+
+  public Optional<List<PhotonTrackedTarget>> getTargets(List<PhotonPipelineResult> allPipelineResults) {
+    
+    for(PhotonPipelineResult result : allPipelineResults) {
+      if (!result.hasTargets()) continue;
+      
+      return Optional.of(result.getTargets());
+    }
+    return Optional.empty(); 
+  }
+
+  private List<PhotonPipelineResult> getAllUnreadResultsPrivite() {
+      List<PhotonPipelineResult> results = m_camera.getAllUnreadResults();
+      return results;
+  };
+
+  public List<PhotonPipelineResult> getAllUnreadResultsStatic(){
+    return currentPipelineResults;
+  }
 
 
   @Override
   public void periodic() {
+    currentPipelineResults = getAllUnreadResultsPrivite();
+
     // This method will be called once per scheduler run
     // Correct pose estimate with vision measurements
     var visionEst = this.getEstimatedGlobalPose();
