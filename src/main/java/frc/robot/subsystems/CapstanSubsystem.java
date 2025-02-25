835a5caa8bae0e467cb829dd30429b729375f50f
Liam Wilmot <114833225+itsameLuigi64@users.noreply.github.com>
2025-02-25 11:39:54 -0500

    Squashed commit of the following:
    commit edd9112f3a14160d7e485c27d8a06fb5b7f50267
    Author: Liam Wilmot <114833225+itsameLuigi64@users.noreply.github.com>
    Date:   Fri Feb 21 11:02:05 2025 -0500
    
        added more motors to capstan to reflect pysical changes
    
    commit 20018850dea54e9a1dccdb062848f0733c4e8165
    Author: Liam Wilmot <114833225+itsameLuigi64@users.noreply.github.com>
    Date:   Thu Feb 6 11:00:14 2025 -0500
    
        Add kNet constant to CapstanSubsystem and update setpoint handling

--- src/main/java/frc/robot/subsystems/CapstanSubsystem.java
+++ src/main/java/frc/robot/subsystems/CapstanSubsystem.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
@@ -16,9 +17,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Configs.Capstan;
-import frc.robot.Constants.CapstanConstants;
-import frc.robot.Constants.CapstanConstants.WristSetpoints;
-import frc.robot.Constants.CapstanConstants.ElevatorSetpoints;
+import static frc.robot.Constants.CapstanConstants.*;
 
 public class CapstanSubsystem extends SubsystemBase {
   /** Subsystem-wide setpoints */
@@ -33,38 +32,57 @@ public class CapstanSubsystem extends SubsystemBase {
     kL4;
   }
 
-  private final SparkMax m_elevatorMotor;
-  private final SparkMax m_wristMotor;
+  private final SparkMax m_elevatorLeader;
+  private final SparkMax m_elevatorFollower;
+  private final SparkMaxConfig m_ElevatorFollowerConfig = new SparkMaxConfig();
+
+  private final SparkMax m_algaeWristMotor;
+  private final SparkMax m_coralWristMotor;
 
   private final RelativeEncoder m_elevatorEncoder;
-  private final RelativeEncoder m_wristEncoder;
+  private final RelativeEncoder m_algaeWristEncoder;
+  private final RelativeEncoder m_coralWristEncoder;
 
   private final SparkClosedLoopController m_elevatorPIDController;
-  private final SparkClosedLoopController m_wristPIDController;
+  private final SparkClosedLoopController m_algaeWristPIDController;
+  private final SparkClosedLoopController m_coralWristPIDController;
 
   private final DigitalInput m_hallSensor;
 
   private boolean wasResetByLimit = false;
   private double elevatorCurrentTarget = ElevatorSetpoints.kStore;
-  private double wristCurrentTarget = WristSetpoints.kStore;
+  private double algaeWristCurrentTarget = AlgaeWristSetpoints.kStore;
+  private double coralWristCurrentTarget = CoralWristSetpoints.kStore;
   private Setpoint currentSetpoint = Setpoint.kStore;
 
   /** Creates a new CapstanSubsystem. */
   public CapstanSubsystem() {
-    m_elevatorMotor = new SparkMax(CapstanConstants.kElevatorCanId, MotorType.kBrushless);
-    m_wristMotor = new SparkMax(CapstanConstants.kWristCanId, MotorType.kBrushless);
+    m_elevatorLeader = new SparkMax(kElevatorLeaderCanId, MotorType.kBrushless);
+    m_elevatorFollower = new SparkMax(kElevatorFollowerCanId, MotorType.kBrushless);
 
-    m_elevatorEncoder = m_elevatorMotor.getEncoder();
-    m_wristEncoder = m_wristMotor.getEncoder();
+    m_algaeWristMotor = new SparkMax(kAlgaeWristCanId, MotorType.kBrushless);
+    m_coralWristMotor = new SparkMax(kCoralWristCanId, MotorType.kBrushless);
 
-    m_elevatorPIDController = m_elevatorMotor.getClosedLoopController();
-    m_wristPIDController = m_elevatorMotor.getClosedLoopController();
+    m_elevatorEncoder = m_elevatorLeader.getEncoder();
+    m_algaeWristEncoder = m_algaeWristMotor.getEncoder();
+    m_coralWristEncoder = m_coralWristMotor.getEncoder();
+    
+    m_elevatorPIDController = m_elevatorLeader.getClosedLoopController();
+    m_algaeWristPIDController = m_algaeWristMotor.getClosedLoopController();
+    m_coralWristPIDController = m_coralWristMotor.getClosedLoopController();
 
-    m_elevatorMotor.configure(Capstan.elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_wristMotor.configure(Capstan.wristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_ElevatorFollowerConfig
+      .apply(Capstan.elevatorConfig)
+      .follow(kElevatorLeaderCanId)
+      .inverted(true);
+    
+    m_elevatorLeader.configure(Capstan.elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_elevatorFollower.configure(m_ElevatorFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_algaeWristMotor.configure(Capstan.algaeWristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_coralWristMotor.configure(Capstan.coralWristConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     m_elevatorEncoder.setPosition(0);
-    //m_wristEncoder.setPosition(0);
+    // m_wristEncoder.setPosition(0);
 
     m_hallSensor = new DigitalInput(0);
 
@@ -79,9 +97,9 @@ public class CapstanSubsystem extends SubsystemBase {
   }
 
   private void moveToSetpoint() {
-    m_wristPIDController.setReference(wristCurrentTarget, ControlType.kMAXMotionPositionControl);
-    m_elevatorPIDController.setReference(
-        elevatorCurrentTarget, ControlType.kMAXMotionPositionControl);
+    m_algaeWristPIDController.setReference(algaeWristCurrentTarget, ControlType.kMAXMotionPositionControl);
+    m_coralWristPIDController.setReference(coralWristCurrentTarget, ControlType.kMAXMotionPositionControl);
+    m_elevatorPIDController.setReference(elevatorCurrentTarget, ControlType.kMAXMotionPositionControl);
   }
 
   /** Zero the elevator encoder when the limit switch is pressed. */
@@ -97,8 +115,9 @@ public class CapstanSubsystem extends SubsystemBase {
     }
   }
 
-    /**
-   * Command to set the subsystem setpoint. This will set the arm and elevator to their predefined
+  /**
+   * Command to set the subsystem setpoint. This will set the arm and elevator to
+   * their predefined
    * positions for the given setpoint.
    */
   public Command setSetpointCommand(Setpoint setpoint) {
@@ -106,40 +125,48 @@ public class CapstanSubsystem extends SubsystemBase {
         () -> {
           switch (setpoint) {
             case kStore:
-              wristCurrentTarget = WristSetpoints.kStore;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kStore;
+              coralWristCurrentTarget = CoralWristSetpoints.kStore;
               elevatorCurrentTarget = ElevatorSetpoints.kStore;
               currentSetpoint = Setpoint.kStore;
             case kProcessor:
-              wristCurrentTarget = WristSetpoints.kProcessor;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kProcessor;
+              coralWristCurrentTarget = CoralWristSetpoints.kProcessor;
               elevatorCurrentTarget = ElevatorSetpoints.kProcessor;
               currentSetpoint = Setpoint.kProcessor;
             case kFeederStation:
-              wristCurrentTarget = WristSetpoints.kFeederStation;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kFeederStation;
+              coralWristCurrentTarget = CoralWristSetpoints.kFeederStation;
               elevatorCurrentTarget = ElevatorSetpoints.kFeederStation;
               currentSetpoint = Setpoint.kFeederStation;
               break;
             case kNet:
-              wristCurrentTarget = WristSetpoints.kProcessor;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kProcessor;
+              coralWristCurrentTarget = CoralWristSetpoints.kProcessor;
               elevatorCurrentTarget = ElevatorSetpoints.kProcessor;
               currentSetpoint = Setpoint.kProcessor;
               break;
             case kL1:
-              wristCurrentTarget = WristSetpoints.kL1;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kL1;
+              coralWristCurrentTarget = CoralWristSetpoints.kL1;
               elevatorCurrentTarget = ElevatorSetpoints.kL1;
               currentSetpoint = Setpoint.kL1;
               break;
             case kL2:
-              wristCurrentTarget = WristSetpoints.kL2;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kL2;
+              coralWristCurrentTarget = CoralWristSetpoints.kL2;
               elevatorCurrentTarget = ElevatorSetpoints.kL2;
               currentSetpoint = Setpoint.kL2;
               break;
             case kL3:
-              wristCurrentTarget = WristSetpoints.kL3;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kL3;
+              coralWristCurrentTarget = CoralWristSetpoints.kL3;
               elevatorCurrentTarget = ElevatorSetpoints.kL3;
               currentSetpoint = Setpoint.kL3;
               break;
             case kL4:
-              wristCurrentTarget = WristSetpoints.kL4;
+              algaeWristCurrentTarget = AlgaeWristSetpoints.kL4;
+              coralWristCurrentTarget = CoralWristSetpoints.kL4;
               elevatorCurrentTarget = ElevatorSetpoints.kL4;
               currentSetpoint = Setpoint.kL4;
               break;
@@ -147,7 +174,6 @@ public class CapstanSubsystem extends SubsystemBase {
         });
   }
 
-  
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
